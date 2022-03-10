8f4be8919683a366ca6153c0f2d5a119c19a0d53
Bernat Diaz <bernatdiaz94@gmail.com>
2022-03-10 09:17:12 +0100

    graficas en movimiento

--- apps/static/assets/js/plot.js
+++ apps/static/assets/js/plot.js
@@ -1,3 +1,7 @@
+const STREAM_WIDTH = 10;
+var count_acc = 1;
+var count_gyr = 1;
+var count_mag = 1;
 function buildChart() {
 
     console.log("buildChart")
@@ -88,40 +92,113 @@ function buildChart() {
         };
         Plotly.newPlot('mag', data_mag, layout, {responsive: true});
 
-        Plotly.newPlot('acc_stream', [{
-            y: [acc_x[0]],
-            mode: 'lines',
-            line: {color: '#42adf5'},
-            name: "Acelerometro X"
-        }, {
-            y: [acc_y[0]],
-            mode: 'lines',
-            line: {color: '#f58742'},
-            name: "Acelerometro Y"
-        }, {
-            y: [acc_z[0]],
-            mode: 'lines',
-            line: {color: '#42f56c'},
-            name: "Acelerometro Z"
-        }]);
 
-        const STREAM_WIDTH = 10;
-        var count_acc = 1;
 
-        var interval_acc = setInterval(function(){
-            Plotly.extendTraces('acc_stream', {
-                y: [[acc_x[count_acc]], [acc_y[count_acc]], [acc_z[count_acc]]]
-            }, [0, 1, 2])
+        Plotly.newPlot('acc_stream', get_initial_data(item, acc_x, acc_y, acc_z, "Acelerometro"))
 
-            if(count_acc > STREAM_WIDTH){
-                Plotly.relayout('acc_stream', {
-                    xaxis : {
-                        range: [count_acc - STREAM_WIDTH, count_acc]
-                    }
-                })
-            }
-            if(++count_acc >= acc_x.length) clearInterval(interval_acc);
-        }, 200)
+        var interval_acc = create_interval(item, acc_x, acc_y, acc_z, "count_acc", "acc_stream");
+
+        const play_acc = document.getElementById("play_acc");
+        play_acc.onclick = function(){
+            interval_acc = create_interval(item, acc_x, acc_y, acc_z, "count_acc", "acc_stream");
+        }
+        const stop_acc = document.getElementById("stop_acc");
+        stop_acc.onclick = function(){
+            clearInterval(interval_acc)
+            count_acc = 1
+            Plotly.react('acc_stream', get_initial_data(item, acc_x, acc_y, acc_z, "Acelerometro"))
+        }
+
+        const pause_acc = document.getElementById("pause_acc");
+        pause_acc.onclick = function(){
+            clearInterval(interval_acc)
+        }
+
+
+
+        Plotly.newPlot('gyr_stream', get_initial_data(item, gyr_x, gyr_y, gyr_z, "Giroscopio"))
+
+        var interval_gyr = create_interval(item, gyr_x, gyr_y, gyr_z, "count_gyr", "gyr_stream");
+
+        const play_gyr = document.getElementById("play_gyr");
+        play_gyr.onclick = function(){
+            interval_gyr = create_interval(item, gyr_x, gyr_y, gyr_z, "count_gyr", "gyr_stream");
+        }
+        const stop_gyr = document.getElementById("stop_gyr");
+        stop_gyr.onclick = function(){
+            clearInterval(interval_gyr)
+            count_gyr = 1
+            Plotly.react('gyr_stream', get_initial_data(item, gyr_x, gyr_y, gyr_z, "Giroscopio"))
+        }
+
+        const pause_gyr = document.getElementById("pause_gyr");
+        pause_gyr.onclick = function(){
+            clearInterval(interval_gyr)
+        }
+
+
+
+        Plotly.newPlot('mag_stream', get_initial_data(item, gyr_x, gyr_y, gyr_z, "Magnetometro"))
+
+        var interval_mag = create_interval(item, gyr_x, gyr_y, gyr_z, "count_mag", "mag_stream");
+
+        const play_mag = document.getElementById("play_mag");
+        play_mag.onclick = function(){
+            interval_mag = create_interval(item, mag_x, mag_y, mag_z, "count_mag", "mag_stream");
+        }
+        const stop_mag = document.getElementById("stop_mag");
+        stop_mag.onclick = function(){
+            clearInterval(interval_mag)
+            count_mag = 1
+            Plotly.react('mag_stream', get_initial_data(item, mag_x, mag_y, mag_z, "Magnetometro"))
+        }
+
+        const pause_mag = document.getElementById("pause_mag");
+        pause_mag.onclick = function(){
+            clearInterval(interval_mag)
+        }
     });
+}
+function get_initial_data(item, x, y, z, name){
+    const data = [{
+        x: [item[0]],
+        y: [x[0]],
+        mode: 'lines',
+        line: {color: '#42adf5'},
+        name: name + " X"
+    }, {
+        x: [item[0]],
+        y: [y[0]],
+        mode: 'lines',
+        line: {color: '#f58742'},
+        name: name + " Y"
+    }, {
+        x: [item[0]],
+        y: [z[0]],
+        mode: 'lines',
+        line: {color: '#42f56c'},
+        name: name + " Z"
+    }];
+    return data;
+}
+function create_interval(item, x, y, z, count_name, div){
+    var interval = setInterval(function(){
+        const count = window[count_name]
+        Plotly.extendTraces(div, {
+            x: [[item[count]], [item[count]], [item[count]]],
+            y: [[x[count]], [y[count]], [z[count]]]
+        }, [0, 1, 2])
+
+        if(item[count] > STREAM_WIDTH){
+            Plotly.relayout(div, {
+                xaxis : {
+                    range: [item[count] - STREAM_WIDTH, item[count]]
+                }
+            })
+        }
+        window[count_name] = window[count_name] + 1
+        if(window[count_name] >= x.length) clearInterval(interval);
+    }, 200)
+    return interval
 }
 buildChart()
